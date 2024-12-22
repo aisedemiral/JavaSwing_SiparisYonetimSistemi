375984715840b579d4a5f37eb8b9663ca3fa244a
aisedemiral <mumineaise@gmail.com>
2024-12-22 20:26:42 +0300

    first commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import core.Database;
 import core.Helper;
+=======
+import business.UserController;
+import core.Database;
+import core.Helper;
+import entity.User;
+import view.DashboardUI;
+>>>>>>> b7fe936 (first commit)
 import view.LoginUI;
 
 import java.sql.Connection;
@@ -8,7 +16,14 @@ import java.sql.Connection;
 public class App {
     public static void main(String[] args){
         Helper.setTheme();
+<<<<<<< HEAD
         LoginUI loginUI = new LoginUI();
+=======
+        UserController userController = new UserController();
+        User user=userController.findByLogin("aise@gmail.com","123123");
+        DashboardUI dashboardUI=new DashboardUI(user);
+        //LoginUI loginUI = new LoginUI();
+>>>>>>> b7fe936 (first commit)
 
 
     }
--- src/view/DashboardUI.java
+++ src/view/DashboardUI.java
@@ -1,5 +1,6 @@
 package view;
 
+<<<<<<< HEAD
 import core.Helper;
 import entity.User;
 
@@ -11,6 +12,47 @@ public class DashboardUI extends JFrame {
     private User user;
     public DashboardUI(User user) {
         this.user = user;
+=======
+import business.CustomerController;
+import core.Helper;
+import entity.Customer;
+import entity.User;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+public class DashboardUI extends JFrame {
+    private JPanel container;
+    private JPanel pnl_top;
+    private JLabel lbl_welcome;
+    private JButton btn_logout;
+    private JTabbedPane tabbedPane1;
+    private JPanel pnl_customer;
+    private JScrollPane scrl_customer;
+    private JTable tbl_customer;
+    private JPanel pnl_customer_filter;
+    private JTextField fld_f_customer_name;
+    private JComboBox cmb_customer_type;
+    private JButton btn_customer_filter;
+    private JButton btn_customer_filter_reset;
+    private JButton btn_customer_new;
+    private JLabel lbl_f_customer_name;
+    private JLabel lbl_filter_customer_type;
+    private User user;
+    private  CustomerController customerController;
+    private DefaultTableModel tmdl_customer=new DefaultTableModel();
+    private JPopupMenu popup_customer =new JPopupMenu();
+
+    public DashboardUI(User user) {
+        this.user = user;
+        this.customerController = new CustomerController();
+>>>>>>> b7fe936 (first commit)
         if (user==null){
             Helper.showMsg("error");
             dispose();
@@ -24,6 +66,63 @@ public class DashboardUI extends JFrame {
         this.setLocation(x,y);
         this.setVisible(true);
 
+<<<<<<< HEAD
         System.out.println(this.user.toString());
+=======
+    this.lbl_welcome.setText("Hoşgeldin:  "+this.user.getName());
+        this.btn_logout.addActionListener(e-> {
+            dispose();
+            LoginUI loginUI=new LoginUI();
+        });
+        loadCustomerTable(null);
+        loadCustomerPopupMenu();
+
+    }
+    private void loadCustomerPopupMenu() {
+
+        this.tbl_customer.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                int selectedRow=tbl_customer.rowAtPoint(e.getPoint());
+                tbl_customer.setColumnSelectionInterval(selectedRow,selectedRow);
+            }
+        });
+
+        this.popup_customer.add("Güncelle").addActionListener(e->{
+            int selecteId= (int) tbl_customer.getValueAt(tbl_customer.getSelectedRow(),0);
+            System.out.println(selecteId);
+       });
+        this.popup_customer.add("Sil").addActionListener(e->{
+            System.out.println("Sil Tıklandı");
+        });
+        this.tbl_customer.setComponentPopupMenu(this.popup_customer);
+    }
+    private void loadCustomerTable(ArrayList<Customer> customers) {
+        Object[] columnCustomer={"id","name","type","phone","mail","address"};
+        if (customers == null) {
+
+            customers=this.customerController.findAll();
+        }
+        //TAblo Sıfırlama kısmı
+        DefaultTableModel model = new DefaultTableModel(columnCustomer, 0);
+        this.tbl_customer.setModel(model);
+
+        // Veriyi ekle
+        for (Customer customer : customers) {
+            Object[] row = {
+                    customer.getId(),
+                    customer.getName(),
+                    customer.getType().name(),
+                    customer.getPhone(),
+                    customer.getMail(),
+                    customer.getAddress()
+            };
+            model.addRow(row);
+        }
+
+        // Sütun düzenlemeleri
+        this.tbl_customer.getColumnModel().getColumn(0).setPreferredWidth(50); // ID sütunu genişliği
+        this.tbl_customer.setEnabled(true);
+>>>>>>> b7fe936 (first commit)
     }
 }
